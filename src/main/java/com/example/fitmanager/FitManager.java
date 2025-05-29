d38ad9dac4eac139b74ed8c5d4501884a0ac4760
2North <nikita552003@gmail.com>
2025-05-29 18:22:23 +0300

    Project & Spring setup

---
+++ src/main/java/com/example/fitmanager/FitManager.java
@@ -0,0 +1,11 @@
+package com.example.fitmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitManager {
+    public static void main(String[] args) {
+        SpringApplication.run(FitManager.class, args);
+    }
+}
